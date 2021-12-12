7777788a925b289350b93ba43919bba0f45c4e1c
tom perkins <t.perk@live.com.au>
2021-12-12 11:48:21 +1100

    fixed lots of bugs

--- src/index.js
+++ src/index.js
@@ -31,12 +31,56 @@ class App extends React.Component{
   }
 
   async equals(){
-    if(this.state.lastInput === ('AC' || 'equals')){
+    if(['AC','equals'].includes(this.state.lastInput)){
       return; 
     }
+    
+    //find number with most decimal places
+    const numWithMostDecimalPlaces = this.state.formula.reduce((a,b)=>{
+      // find how many places a has after decimal point
+      let aLength = 0;
+      let bLength = 0;
+      a = String(a);
+      if(a.includes('.')){
+        const sp = a.split('.');
+        aLength = sp[1].length;
+      }
+      if(b.includes('.')){
+        const sp = b.split('.');
+        bLength = sp[1].length;
+      }
+
+      return (aLength > bLength)? a:b;
+    });
+
+    //find the length of decimal places in that num
+    let decimalPlaces = 2;
+    if(numWithMostDecimalPlaces.includes('.')){
+      const sp = numWithMostDecimalPlaces.split('.');
+      decimalPlaces = sp[1].length;
+      
+    }
+
+    let significantPlaces = (decimalPlaces > 2)? decimalPlaces : 2;
+    
     const expr = this.state.formula.join(' ');
     console.log(expr)
-    const val = Math.floor(eval(expr)*100)/100;
+    let answer = eval(expr);
+    // find leading 0 of answer so they arnt rounded off
+    answer = String(answer);
+    let answerSplit = answer.split('.');
+    if(answerSplit[0] === '0'){
+      //found fraction below 1
+      let count = 0;
+      answerSplit[1].split('').some((digit)=>{
+        count += 1;
+        return Number(digit) > 0
+      })
+      significantPlaces = (count >= significantPlaces) ? count + 1: significantPlaces;
+    } 
+    const placesFactor = (significantPlaces > 2)? 10**significantPlaces: 10**2; 
+    
+    const val = Math.floor(answer*placesFactor)/placesFactor;
     await this.setState((previousState)=>{
       return({
         ...previousState,
@@ -88,15 +132,16 @@ class App extends React.Component{
         });
         break;
       default:
+        const id = (e.target.id === 'zero') ? '0' : e.target.id;
         await this.setState((previousState)=>{
-          let val = (['AC', 'equals', '-','*','+','/'].includes(previousState.lastInput)) ? e.target.id : previousState.display + e.target.id; 
-          val = (e.target.id === 'zero')? '0':val;
-          let form = (['AC', 'equals', '-','*','+','/'].includes(previousState.lastInput))? [...previousState.formula] : [...previousState.formula.slice(0,-1)];
+          let val = (['AC', 'equals', '-','*','+','/'].includes(previousState.lastInput) || previousState.display === '0') ? id : previousState.display + id;          
+          let form = (['AC', '-','*','+','/'].includes(previousState.lastInput))? [...previousState.formula] : [...previousState.formula.slice(0,-1)];
+          form = (previousState.lastInput === 'equals') ? [] : form;
           return({
             ...previousState,
             display: val,
             formula: [...form, val],
-            lastInput: e.target.id,
+            lastInput: id,
           })
         })
     }
